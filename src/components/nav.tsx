f07fff7eb85b73e12ecffb7d00a22b69e6314de0
al subzy <zapdirzak@gmail.com>
2025-12-21 16:24:52 +0000

    ## Error Type Runtime Error
    ## Error Message
    React.Children.only expect

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -383,10 +383,10 @@ export default function Nav() {
                         isActive={pathname === subItem.href}
                       >
                         <Link href={subItem.href}>
-                            <div className="flex items-center gap-2">
-                                <subItem.icon className="h-4 w-4" />
-                                <span>{subItem.label}</span>
-                            </div>
+                          <div className="flex items-center gap-2">
+                            <subItem.icon className="h-4 w-4" />
+                            <span>{subItem.label}</span>
+                          </div>
                         </Link>
                       </SidebarMenuSubButton>
                     </SidebarMenuItem>
